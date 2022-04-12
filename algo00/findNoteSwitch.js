a6ddce12615f5ad7e5aeaf671fe57f2f1225044b
anilraj <anil050797@gmial.com>
2022-04-12 12:56:16 +0530

    swith function

---
+++ algo00/findNoteSwitch.js
@@ -0,0 +1,30 @@
+// how much not does we have 100, 50, 20, 1
+
+
+const findNote = (r) => {
+    switch (1) {
+        case 1: {
+            let Rs100 = Math.floor(r / 100);
+            r = r % 100                       // updating the the amount 
+            console.log(" Rs100 note:" + Rs100);
+            // will not break here, need to deal with multiple statements
+        }
+        case 2: {
+            let Rs50 = Math.floor( r / 50);
+            r = r % 50
+            console.log(" Rs50 note:" + Rs50);
+        }
+        case 3: {
+            let Rs20 = Math.floor (r / 20);
+            r = r % 20
+            console.log(" Rs20 note:" + Rs20);
+        }
+        default: {
+            let Rs1 =Math.floor(r / 1) ;
+            r = r % 1
+            console.log(" Rs1 note:" + Rs1);
+        }
+    }
+}
+const rupay = 1523648;
+findNote(rupay);
--- algo00/reverseInt.js
+++ algo00/reverseInt.js
@@ -1,22 +1,19 @@
 // not working
 
 const reverseInt = (n) => {
-    let ans = 0;
-    let i = 0;
+    let ans = 0
     while(n!=0){
         let digit = n%10;   //3, 2, 1
-
+        
         // condition, if number is not in range of int
         // if((ans> int_max/10) || (ans<int_min/10)){
         //     return 0
         // }
-        ans = ans*10 + digit; // 3, 3*10+ 2=32, 32*10+1=321 
-        n = n/ 1;
-        i ++;
-       
+        ans = (ans*10) + digit; // 3, 3*10+ 2=32, 32*10+1=321 
+        n = n/10;
     }
     return ans;
-    console.log(ans)
+    
 }
 const number = 123;
 const result = reverseInt(number);
