5aa3e64d5a07df6befdcf95aaf88d29577913b03
airKing05 <anil050797@gmail.com>
2022-06-19 14:19:38 +0530

    name changed of babar01-babbar01

--- prectice.js
+++ prectice.js
@@ -3,12 +3,8 @@
 
 
 function bToD(arr, k) {
-  let len = arr.length;
- for (let i=0; i<len; i++){
-  [arr[k], arr[k*2]] = [arr[k*2], arr[k]]
- }
-return arr;
+     
 }
 let array = [1, 3, 2, 4,12, 5,1,3, 0];
 const result = bToD(array, 2);
-console.log(result)
+console.log("result",result)
